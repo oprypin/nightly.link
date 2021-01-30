a5123dabfeb091e8b7032e5e336cbdbb8baa1ed7
Oleh Prypin <oleh@pryp.in>
2021-01-30 19:01:45 +0100

    Suppress exception details in log

--- nightly_link.cr
+++ nightly_link.cr
@@ -502,7 +502,7 @@ end
 struct ART::Listeners::Error
   protected def log_exception(exception : Exception, &block : -> String) : Nil
     if exception.is_a? ART::Exceptions::NotFound
-      LOGGER.warn { exception }
+      LOGGER.warn { "Not found" }
     else
       previous_def(exception, &block)
     end
