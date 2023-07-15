09af42a95ca5c5b6d2bc69ff47a63ccd8298f1d3
Oleh Prypin <oleh@pryp.in>
2023-07-15 13:06:12 +0200

    Generate anchors for headings

--- src/render_md.cr
+++ src/render_md.cr
@@ -1,3 +1,5 @@
 require "markd"
 
-print Markd.to_html(ARGF.gets_to_end).gsub(/<include (\S+)>/, %(<% ECR.embed("\#{__DIR__}/templates/\\1.html", ctx.response) %>))
+print Markd.to_html(ARGF.gets_to_end)
+  .gsub(/^<(h[1-6])>([^<>]+?)<\/\1>$/m) { %(<#{$1} id="#{$2.gsub(/\W+/, "-").downcase}">#{$2}</#{$1}>) }
+  .gsub(/<include (\S+)>/, %(<% ECR.embed("\#{__DIR__}/templates/\\1.html", ctx.response) %>))
